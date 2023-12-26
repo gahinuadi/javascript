f799438483de0a352a79db2e930269235e5a9452
Viraj <gahininathadalinge@gmail.com>
2023-12-26 10:22:36 +0530

    variable assignment file
    variable assignment file

---
+++ 01_variableAssign.js
@@ -0,0 +1,29 @@
+ console.log("Before Initialization");
+
+ var FullName;
+ console.log("FullName:",FullName);
+
+ var FullName = "Gahininath Adalinge";
+ console.log("FullName:", FullName);
+
+ var FullName = "Gahininath Annaso Adalinge";
+ console.log("CompleteName:",FullName);
+
+ var myLove = "I Love Only JS";
+ console.log(myLove);
+
+ var firstName = "Gahininath";
+ var lastName = "Adalinge";
+ var age = "27";
+ var adress = "Pune";
+ var pinCode = "412308";
+ var gender = "Male";
+ var isMarried = "No";
+
+ console.log("First Name:",firstName);
+ console.log("Last Name",lastName);
+ console.log("Age",age);
+ console.log("Adress",adress);
+ console.log("Pin Code",pinCode);
+ console.log("Gender",gender);
+ console.log("Is Married",isMarried);
---
+++ helloWord.js
@@ -0,0 +1,18 @@
+console.log("Hello JS, I Am Gahininath");
+
+var num;
+console.log(num = "10");
+
+var num="10";
+console.log(num);
+
+console.log("Gahininath");
+
+var myname="Gahininath";
+console.log(myname);
+
+console.log("first name",myname);
+
+myName="Gahininath Adalinge";
+console.log("Full name",myName);
+
