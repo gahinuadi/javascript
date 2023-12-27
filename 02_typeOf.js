8d03df382f22f69706e9944a5da9fe839fa484cf
Viraj <gahininathadalinge@gmail.com>
2023-12-27 15:18:31 +0530

    variable ,data type and typeof operater file
    variable ,data type and typeof operater file

---
+++ 02_typeOf.js
@@ -0,0 +1,21 @@
+//-----------01 assign ---------//
+var bankName;
+console.log("Bank Name:",bankName,",","Data Type Of Operater:",typeof bankName);
+
+var bankName = 'SBI Bank';
+console.log("Bank Name:",bankName,",","Data Type Of Operater:",typeof bankName);
+//----------02 assign---------//
+var marks = '90%';
+console.log("Marks:",marks,",","Data Type Of Operater:",typeof marks);
+//---------03----------------//
+var isWorking;
+console.log("Is Working:",isWorking,",","Data Type Of Operater:",typeof isWorking);
+
+var isWorking = true;
+console.log("Is Working:",isWorking,",","Data Type of Operter:",typeof isWorking);
+//--------04 assign----------//
+var totalCount = "Hundred and Seven";
+console.log("Total Count:",totalCount,",","Data Type Of Operater:",typeof totalCount);
+
+var totalCount = 107;
+console.log("Total Count:",totalCount,",","Data Type Of Operater:",typeof totalCount);
