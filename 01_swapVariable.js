ff6e2690bd09525fe73cb8a24c49fd4935fa7cf7
Viraj <gahininathadalinge@gmail.com>
2023-12-27 15:52:26 +0530

    Swaping Variables
    Swaping Variables

--- 01_swapVariable.js
+++ 01_swapVariable.js
@@ -1,14 +1,35 @@
+var sweety = "Sweety";
+var cutie = "Cutie";
+
+console.log("Before Swap:");
+console.log("Sweety->",sweety);
+console.log("Cutie->",cutie);
+
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+
+console.log("After swap:");
+console.log("Sweety->",sweety);
+console.log("Cutie->",cutie);
+
+
+console.log("---------- 02 Question-------------");
 var n1 = "10";
 var n2 = "20";
+var n3 = "30"
 
 console.log("before swap:");
 console.log("n1->",n1);
 console.log("n2->",n2);
+console.log("n3->",n3);
 
 var temp = n1;
 n1 = n2;
-n2 = temp;
+n2 = n3;
+n3 = temp;
 
 console.log("After swap:");
 console.log("n1->",n1);
 console.log("n2->",n2);
+console.log("n3->",n3);
