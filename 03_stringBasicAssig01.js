827301f2e5265914348ec40bb2c8c49ce101a5c5
Viraj <gahininathadalinge@gmail.com>
2024-01-04 02:11:27 +0530

    Assignment 03 string basic assig 01 file
    Assignment 03 string basic assig 01

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,14 @@
+console.log("==================Assignment 01===================");
+
+function stringBasic(){
+    var company = "Google";
+    console.log("My Dream Company is=", company);
+}
+stringBasic();
+
+var hobby1="Reading Books";
+var hobby2="Travelling";
+var hobby3="Playing Cricket";
+console.log(`My Hobbies Are=${hobby1}, ${hobby2}, ${hobby3}`);
+var totalChar=hobby1.length + hobby2.length + hobby3.length;
+console.log(`Sum the total number of character is= ${totalChar}`);
