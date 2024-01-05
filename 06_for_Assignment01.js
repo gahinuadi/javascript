9bd348e75873fbfd019a1c216e8fdef3a6f8a9e9
Viraj <gahininathadalinge@gmail.com>
2024-01-05 18:00:29 +0530

    Assignment 06 for loop file
    Assignment 06 for loop file

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,48 @@
+console.log("===============step 1================");
+
+for (var index = 5; index <=15; index++) {
+    console.log(index);
+    
+}
+
+console.log("============step 2==============");
+
+for (var index = 50; index >= 40; index--) {
+    console.log(index);
+    
+}
+
+console.log("============step 3================");
+
+for (var index = 1; index <= 30; index= index+2) {
+    console.log(index);
+    
+}
+
+console.log("==============step 4============");
+
+for (let index = 2; index <=20; index=index+2) {
+    console.log(index);
+    
+}
+
+console.log("=========step 5============");
+
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);
+    
+}
+
+console.log("=================step 6===============");
+
+for (let index = 10; index <= 100; index=index+10) {
+    console.log(index);
+    
+}
+
+console.log("==============step 7===============");
+
+for (let index = 100; index >= 10; index=index-10) {
+    console.log(index);
+    
+}
---
+++ 06_marriageEligibility.js
@@ -0,0 +1,5 @@
+var marriageCheker = function(marriage){
+    if () {
+        
+    }
+}
